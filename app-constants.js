7476cd9bb72d47fd4e509c4fa3077f1c8ff0592d
Aditya Gopal <36183939+adityagopal@users.noreply.github.com>
2023-03-16 14:16:05 +0530

    feat: query by email (#105) (#106)
    * feat: query by email (#105)
    
    * feat: query by email
    
    * Added tgadmin to constants
    
    * UnauthorizedError for No Token on query via Email
    
    * fix: restrict search by email (#107)
    
    * fix: restrict search by email
    
    - restrict to users with roles: admin, administrator & tgadmin
    
    * fix: typo
    
    ---------
    
    Co-authored-by: Rakib Ansary <rakib.saikot@colorado.edu>

--- app-constants.js
+++ app-constants.js
@@ -2,6 +2,7 @@
  * App constants
  */
 const ADMIN_ROLES = ['administrator', 'admin']
+const SEARCH_BY_EMAIL_ROLES = ADMIN_ROLES.concat('tgadmin');
 const AUTOCOMPLETE_ROLES = ['copilot', 'administrator', 'admin', 'Connect Copilot', 'Connect Account Manager', 'Connect Admin', 'Account Executive']
 
 const EVENT_ORIGINATOR = 'topcoder-member-api'
@@ -29,6 +30,7 @@ const MAMBO_GET_REWARDS_ALLOWED_FIELDS = [
 
 module.exports = {
   ADMIN_ROLES,
+  SEARCH_BY_EMAIL_ROLES,
   AUTOCOMPLETE_ROLES,
   EVENT_ORIGINATOR,
   EVENT_MIME_TYPE,
