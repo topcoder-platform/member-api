e008b31a5a1da1bef182df4f716ed85b51fc45af
Justin Gasper <jmgasper@gmail.com>
2023-12-07 21:59:52 +1100

    Tweak logging again to better match cloudwatch

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -284,8 +284,7 @@ async function fillMembers(docsMembers, query, fields, skillSearch=false) {
     results = _.map(docsMembers.hits.hits, (item) => item._source)
     // TODO: Remove this line after debugging completes.  This line logs 
 
-    logger.info("Results before fill:")
-    logger.info(JSON.stringify(results.slice(0,5)))
+    logger.info("Results before fill: " + JSON.stringify(results.slice(0,5)))
 
     // search for a list of members
     query.handlesLower = _.map(results, 'handleLower')
@@ -315,17 +314,14 @@ async function fillMembers(docsMembers, query, fields, skillSearch=false) {
       results = await addVerifiedFlag(results)
     }
     // TODO: Remove after debugging
-    logger.info("Before filter:")
-    logger.info(JSON.stringify(results.slice(0,10)))
+    logger.info("Before filter: " + JSON.stringify(results.slice(0,10)))
     // filter member based on fields
     results = _.map(results, (item) => _.pick(item, fields))
     // TODO: Remove after debugging
-    logger.info("After filter:")
-    logger.info(JSON.stringify(results.slice(0,10)))
+    logger.info("After filter: " + JSON.stringify(results.slice(0,10)))
   }
   // TODO: Remove after debugging
-  logger.info("Result from fill:")
-  logger.info(JSON.stringify(results.slice(0,10)))
+  logger.info("Result from fill: " + JSON.stringify(results.slice(0,10)))
   
   return { total: total, page: query.page, perPage: query.perPage, result: results }
 }
